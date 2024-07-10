8a6fb18d53a0dd39ebbccc1d845aef6a51cce2c6
jeevan0312 <154587716+jeevan0312@users.noreply.github.com>
2024-07-10 21:58:34 +0530

    Add files via upload

---
+++ Exercise4.java
@@ -0,0 +1,20 @@
+public class Exercise4 {
+ public static void main(String[] args) {
+  Car myCar = new Car("toyota");
+  Car.Engine startMyCar = myCar.new Engine();
+  startMyCar.start();
+ }
+}
+class Car{
+ String car;
+
+ Car(String car){
+  this.car = car;
+ }
+
+ class Engine{
+  void start(){
+   System.out.println("your "+car+" car is started");
+  }
+ }
+}
